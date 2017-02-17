394253dff61603c77272d620aab811299df5a170
Vinícius Graeff Sturmer <vinicius.sturmer@digitaldesk.com.br>
2017-02-17 14:54:15 -0200

    Restringindo textbox

--- Site Lista/js/javascript.js
+++ Site Lista/js/javascript.js
@@ -83,3 +83,22 @@ $(document).ready(function(){
     }
 
 });
+
+function isNumberKey(evt)
+       {
+          var charCode = (evt.which) ? evt.which : evt.keyCode;
+          if (charCode != 46 && charCode > 31 
+            && (charCode < 48 || charCode > 57))
+             return false;
+
+          return true;
+       }
+
+function isNumberKey1(evt)
+       {
+          var charCode = (evt.which) ? evt.which : evt.keyCode;
+          if (charCode >= 48 && charCode <= 57)
+             return false;
+
+          return true;
+       }
